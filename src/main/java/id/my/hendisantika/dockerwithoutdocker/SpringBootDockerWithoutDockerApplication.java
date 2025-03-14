e0d105d34c9ac7f659dace02ff8715b2c384386a
hendisantika <hendisantika@yahoo.co.id>
2025-03-15 05:21:55 +0700

    feat: index route API🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/dockerwithoutdocker/SpringBootDockerWithoutDockerApplication.java
+++ src/main/java/id/my/hendisantika/dockerwithoutdocker/SpringBootDockerWithoutDockerApplication.java
@@ -2,12 +2,22 @@ package id.my.hendisantika.dockerwithoutdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
+@RestController
 @SpringBootApplication
 public class SpringBootDockerWithoutDockerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDockerWithoutDockerApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "Spring Boot with Jib Demo! " + LocalDateTime.now();
     }
 
 }
