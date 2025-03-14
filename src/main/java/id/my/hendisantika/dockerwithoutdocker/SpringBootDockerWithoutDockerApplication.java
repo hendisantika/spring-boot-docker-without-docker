9919491e55c251eaa86a55287f8f7436b9eb9de8
hendisantika <hendisantika@yahoo.co.id>
2025-03-15 05:12:51 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/dockerwithoutdocker/SpringBootDockerWithoutDockerApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.dockerwithoutdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerWithoutDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerWithoutDockerApplication.class, args);
+    }
+
+}
