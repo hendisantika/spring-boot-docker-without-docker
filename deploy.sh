8ffb22d83fa76b98ed3d28e2e950c05d0325b51d
hendisantika <hendisantika@yahoo.co.id>
2025-03-21 08:10:08 +0700

    feat: Rename Docker Image name :three:🫸🌀✏️📗🐧🐳

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ssh -p "${SERVER_PORT}" "${SERVER_USERNAME}"@"${SERVER_HOST}" -i key.txt -t -t -o StrictHostKeyChecking=no << 'ENDSSH'
-cd ~/spring-boot-jib
+cd ~/spring-boot-docker-without-docker
 set +a
 source .env
 start=$(date +"%s")
