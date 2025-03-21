134e5ee53396e51a72ec72bfaf76c70328a3197c
hendisantika <hendisantika@yahoo.co.id>
2025-03-21 08:43:11 +0700

    fix: Change $CONTAINER_NAME :three:🫸🌀✏️📗🐧🐳

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
     docker rm $CONTAINER_NAME
 fi
 
-docker run -d --restart unless-stopped -p $APP_SERVER_PORT:$APP_SERVER_PORT --env-file .env --name $CONTAINER_NAME  hendisantika/springboot-jib:$IMAGE_TAG
+docker run -d --restart unless-stopped -p $APP_SERVER_PORT:$APP_SERVER_PORT --env-file .env --name $CONTAINER_NAME  hendisantika/$CONTAINER_NAME:$IMAGE_TAG
 exit
 ENDSSH
 
