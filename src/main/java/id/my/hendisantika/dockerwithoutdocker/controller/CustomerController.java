b9003059e40c9762ba106d7f6bae896ff82dcbaa
hendisantika <hendisantika@yahoo.co.id>
2025-03-21 07:41:35 +0700

    feat: Add CustomerController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/dockerwithoutdocker/controller/CustomerController.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.dockerwithoutdocker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-docker-without-docker
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 21/03/25
+ * Time: 07.41
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping(path = "api/v1/customers")
+public class CustomerController {
+}
